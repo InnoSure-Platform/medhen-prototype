a2330e697d7f67af44374095c5508eef42d9c36c
YasabnehTA-Inno <265933360+YasabnehTA-Inno@users.noreply.github.com>
2026-07-18 13:14:21 +0300

    fix: next-auth type definitions for idToken

--- web/types/next-auth.d.ts
+++ web/types/next-auth.d.ts
@@ -3,6 +3,8 @@ import NextAuth, { DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
+    idToken?: string;
+    error?: string;
     role?: string;
     user: {
       id?: string;
@@ -20,6 +22,8 @@ declare module "next-auth" {
 declare module "next-auth/jwt" {
   interface JWT {
     accessToken?: string;
+    idToken?: string;
+    error?: string;
     role?: string;
   }
 }
